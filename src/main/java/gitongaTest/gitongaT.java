1c79c65a07095c70557ae5d0b67b62e1976357c9
gitonga24 <39718444+gitonga24@users.noreply.github.com>
2019-09-08 18:00:23 -0400

    testing repository functionality

---
+++ src/main/java/gitongaTest/gitongaT.java
@@ -0,0 +1,12 @@
+package gitongaTest;
+
+public class gitongaT {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		System.out.println("Tesing  project output");
+	}
+
+}
